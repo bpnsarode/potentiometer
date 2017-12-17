eb2ca6850947d81d636e2e7d637286135d867e56
Bipin Sarode <bipinsarode738@yahoo.in>
2017-12-17 19:36:26 +0530

    Add files via upload

---
+++ voltage_sampling.ino
@@ -0,0 +1,15 @@
+void setup() 
+{
+  Serial.begin(9600); // initializing serial communication 9600 bits per second:
+}
+
+void loop() 
+{
+  int sensorValue = analogRead(A0); // input 
+  float voltage = sensorValue * (5.0 / 1023.0); // convert sensor analog value into voltages:
+  Serial.println(voltage);
+  delay(10);
+
+}
+
+
